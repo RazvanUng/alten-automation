783be00b2c835cfbe94c848ea25115e97de296e3
Razvan1990 <razvan.ungureanu6@gmail.com>
2021-08-22 23:16:54 +0300

    Course 18/08/2021 -> jsonDataProvider

--- src/test/java/tests/testNgTests/loginTests/UsernameTestsPageObjectMod.java
+++ src/test/java/tests/testNgTests/loginTests/UsernameTestsPageObjectMod.java
@@ -1,6 +1,8 @@
 package tests.testNgTests.loginTests;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
@@ -12,60 +14,66 @@ import utils.Constants;
 import utils.GeneralUtils;
 import utils.Log;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
 import java.io.File;
 import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 
-public class UsernameTestsPageObjectMod  extends BaseClass {
+public class UsernameTestsPageObjectMod extends BaseClass {
 
-    @DataProvider(name ="UsernameDataProvider")
-    public Iterator<Object[]> usernameData(){
+    @DataProvider(name = "UsernameDataProvider")
+    public Iterator<Object[]> usernameData() {
         Collection<Object[]> dp = new ArrayList<>();
-        dp.add(new String[]{"9","Username exceed maximum length","1"});
-        dp.add(new String[]{"3","","1"});
-        dp.add(new String[]{"2","Username below minimum number of characters","1"});
-        dp.add(new String[]{"8","","1"});
-        dp.add(new String[]{"8","Username does not allow special characters","4"});
-        dp.add(new String[]{"8", "Username does not allow alphanumeric characters","6"});
-        dp.add(new String[]{"0","Username is required!","1"});
+        dp.add(new String[]{"9", "Username exceed maximum length", "1"});
+        dp.add(new String[]{"3", "", "1"});
+        dp.add(new String[]{"2", "Username below minimum number of characters", "1"});
+        dp.add(new String[]{"8", "", "1"});
+        dp.add(new String[]{"8", "Username does not allow special characters", "4"});
+        dp.add(new String[]{"8", "Username does not allow alphanumeric characters", "6"});
+        dp.add(new String[]{"0", "Username is required!", "1"});
         return dp.iterator();
 
     }
 
 
     @Test
-    public void basicAuthTest(){
-        driver.get(Constants.URL_BASED2+ "/#/login");
+    public void basicAuthTest() {
+        driver.get(Constants.URL_BASED2 + "/#/login");
         //Page factory line actually initializes all the web elements from the page
-        LoginPage loginPage = PageFactory.initElements(driver,LoginPage.class);
+        LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
         loginPage.login("zebra", "zebrapassword");
     }
 
-    @Test(dataProvider ="UsernameDataProvider")
-    public void authUserLengthTest(String length, String userError, String charsetType){
-        driver.get(Constants.URL_BASED2+ "/#/login");
+    @Test(dataProvider = "UsernameDataProvider")
+    public void authUserLengthTest(String length, String userError, String charsetType) {
+        driver.get(Constants.URL_BASED2 + "/#/login");
         LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
 
-       // Assert.assertTrue(loginPage.checkCurrentPage());
+        // Assert.assertTrue(loginPage.checkCurrentPage());
         String generateUsername = GeneralUtils.getRandomStringByLength(Integer.parseInt(length), Integer.parseInt(charsetType));
-        String generatePassword = GeneralUtils.getRandomStringByLength(8,5);
-        loginPage.login(generateUsername,generatePassword);
+        String generatePassword = GeneralUtils.getRandomStringByLength(8, 5);
+        loginPage.login(generateUsername, generatePassword);
         loginPage.validateUserError(userError);
 
 
-
     }
+
     @DataProvider(name = "jsonDp")
-    public Iterator<Object[]> jsonDp(){
-        Collection<Object[]>dp = new ArrayList<>();
+    public Iterator<Object[]> jsonDp() {
+        Collection<Object[]> dp = new ArrayList<>();
         ObjectMapper mapper = new ObjectMapper();
         File f = new File("src\\test\\resources\\dataFiles\\testdata.json");
         try {
-            LoginModel loginModel = mapper.readValue(f,LoginModel.class);
+            LoginModel loginModel = mapper.readValue(f, LoginModel.class);
             //citeste din fisier un loginModel
-            dp.add(new Object[] {loginModel});
+            dp.add(new Object[]{loginModel});
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -73,15 +81,79 @@ public class UsernameTestsPageObjectMod  extends BaseClass {
     }
 
     @Test(dataProvider = "jsonDp")
-    public void jsonTest(LoginModel loginModel){
-      //  System.out.println(loginModel.getAccount().getUsername());
-       // System.out.println(loginModel.getAccount().getPassword());
+    public void jsonTest(LoginModel loginModel) {
+        //  System.out.println(loginModel.getAccount().getUsername());
+        // System.out.println(loginModel.getAccount().getPassword());
         String username = loginModel.getAccount().getUsername();
         String password = loginModel.getAccount().getPassword();
-        driver.get(Constants.URL_BASED2+"#/login");
-        LoginPage loginPage = PageFactory.initElements(driver,LoginPage.class);
+        driver.get(Constants.URL_BASED2 + "#/login");
+        LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
         loginPage.login(username, password);
         loginPage.validateErrors(loginModel.getUserError(), loginModel.getPasswordError(), loginModel.getGeneralError());
 
+    }
+
+    private LoginModel readModel(File f) {
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(LoginModel.class);
+            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+            return (LoginModel) unmarshaller.unmarshal(f);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    /*xml*/
+    @DataProvider(name = "xmlDataProvider")
+    public Iterator<Object[]> xmlDp() {
+        Collection<Object[]> dp = new ArrayList<>();
+        File f = new File("src\\test\\resources\\dataFiles\\testdata.xml");
+        LoginModel loginModel = readModel(f);
+        dp.add(new Object[]{loginModel});
+        return dp.iterator();
+    }
+
+    @Test(dataProvider = "xmlDataProvider")
+    public void xmlTest(LoginModel loginModel) {
+        String username = loginModel.getAccount().getUsername();
+        String password = loginModel.getAccount().getPassword();
+        //ne conectam la server
+        driver.get(Constants.URL_BASED2 + "#/login");
+        //ne logam
+        LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+        //folosim credentialele de username si password ptr login
+        loginPage.login(username, password);
+        //verificam mesajele de eroare cu functia de validateErros din LoginPage
+        loginPage.validateErrors(loginModel.getUserError(), loginModel.getPasswordError(), loginModel.getGeneralError());
+    }
+
+    /*csv*/
+    @DataProvider(name = "csvDataProvider")
+    public Iterator<Object[]> csvDp() {
+        Collection<Object[]> dp = new ArrayList<>();
+
+        try {
+            File f = new File("src\\test\\resources\\dataFiles\\testdata.csv");
+            Reader reader = Files.newBufferedReader(Paths.get(f.getAbsolutePath()));
+            CSVReader csvReader = new CSVReader(reader);
+            List<String[]> csvData = csvReader.readAll();
+            //practiclina csv e un vector de stringuri
+           dp.addAll(csvData);
+
+            } catch (IOException | CsvException ioException) {
+            ioException.printStackTrace();
+        }
+
+        return dp.iterator();
+    }
+
+    @Test(dataProvider = "csvDataProvider")
+    public void csvTest(String username, String password, String userErr, String passErr, String generalErr){
+        driver.get(Constants.URL_BASED2+"#/login");
+        LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+        loginPage.login(username, password);
+        loginPage.validateErrors(userErr,passErr,generalErr);
     }
 }
--- src/test/java/tests/testNgTests/loginTests/models/LoginModel.java
+++ src/test/java/tests/testNgTests/loginTests/models/LoginModel.java
@@ -4,8 +4,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
 @Getter @Setter
 @NoArgsConstructor
+@XmlRootElement
 public class LoginModel {
 
     private AccountModel account;
